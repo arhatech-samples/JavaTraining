2e9757024da0e26c092117ef3e1c6e7c620163ae
Ankit Kamboj <er.ankitkamboj@gmail.com>
2014-06-08 14:04:50 +0530

    Update Constructors.java
    Questions edited

--- src/main/java/com/techarha/training/java/basics/question/Constructors.java
+++ src/main/java/com/techarha/training/java/basics/question/Constructors.java
@@ -3,11 +3,15 @@ package com.techarha.training.java.basics.question;
 /**
  * @question1 Create a class with a default constructor (one that takes no arguments) that prints a message.
  *           Create an object of this class.
- * @question2 Add an overloaded constructor that takes a String argument and prints it along with your message.
- * @question3 Modify question2 so that two of the overloaded constructors have two arguments (of two different types),
- *            but in reversed order relative to each other. Verify that this works.
+ * @explaination1 Create a class Person and create its object in Constructors class's main method.
  *
- * @explaination: create a main method and create create the objects of class using different constructors
+ * @question2 Add an overloaded constructor that takes a String argument and prints it along with your message.
+ * @explaination2 Create an overloaded constructor in Person.
+ *
+ * @question3 Modify class created for question2 so that 2 more overloaded constructors are created having two arguments each (of two different types),
+ *            but in reversed order relative to each other. Verify that this works.
+ * @explaination3 Create 2 more overloaded constructors in class Person with String and Int type arguments. The newly created constructors should
+ *                have arguments in reverse order relative to each other. ie [(String, int)] in one & [(int, String)] in other.
  *
  * @author ankit.
  */
